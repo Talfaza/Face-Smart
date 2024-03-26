309796abd26e1e446138e15ee78a693b02685ba5
Talfaza <charifyahia4@gmail.com>
2024-03-26 04:12:56 +0000

    add: comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 import os
+#webcam size
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-
+#importing images
 imgBack = cv2.imread('ressources/background.png')
 
 modelsDir = 'ressources/modules'
@@ -15,9 +16,9 @@ for name in modelsPath:
     model_img = cv2.imread(model_path)
     imgModelsList.append(model_img)
 
-print(len(imgModelsList))
+#print(len(imgModelsList))
 while True:
     success, img = cap.read()
-    imgBack[162:162+480,55:55+640] = img
+    imgBack[162:162+480,55:55+640] = img # filling webcam in backgound imgBack[startWidth:endWidth, heigh,heigh]
     cv2.imshow("Face Smart",imgBack)
     cv2.waitKey(1)
